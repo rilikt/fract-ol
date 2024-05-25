5bb0a0e79220ca8624b1d7aabd9c807d656b55ce
Timur Schmidt <timschmi@3-B-2.42heilbronn.de>
2024-05-25 20:24:05 +0200

    added a color palette, mapping from pixel to complex plane needs to be improved (looks a little warped), color palette not working with higher max_iterations (calculations)

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,22 +6,21 @@
 /*   By: timschmi <timschmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 18:32:09 by timschmi          #+#    #+#             */
-/*   Updated: 2024/05/25 19:14:55 by timschmi         ###   ########.fr       */
+/*   Updated: 2024/05/25 20:20:22 by timschmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "frac.h"
 
-int calc_point(t_plane *c)
+int calc_point(t_plane *c, int max_iter)
 {
-	int iterations = 50;
 	int i = 0;
 	t_plane z, temp;
 
 	z.x = 0;
 	z.y = 0;
 
-	while (i < iterations)
+	while (i < max_iter)
 	{
 		temp = z;
 		z.x = (z.x * z.x) - (z.y * z.y) + c->x;
@@ -33,12 +32,30 @@ int calc_point(t_plane *c)
 	return (i);
 }
 
+int generate_color(int iter, int max_iter)
+{
+	if (iter == max_iter)
+		return (0);
+	
+	int color_palette[16] = {	0xFF0000, 0xFF7F00, 0xFFFF00, 0x7FFF00,
+								0x00FF00, 0x00FF7F, 0x00FFFF, 0x007FFF,
+								0x0000FF, 0x7F00FF, 0xFF00FF, 0xFF007F,
+								0xFFFFFF, 0xC0C0C0, 0x808080, 0x404040
+							};
+
+	int index_palette = (iter * 16) / max_iter;
+
+	return (color_palette[index_palette]);
+}
+
 void mandelbrot()
 {
 	// x^2 - y^2 + 2xyi
 
 	void	*mlx;
 	void	*mlx_win;
+	int color;
+	int max_iter = 100;
 	t_data	img;
 
 	mlx = mlx_init();
@@ -57,14 +74,11 @@ void mandelbrot()
 		pixel_y = 0;
 		while (pixel_y < 1000)
 		{
-			c.x = -2 + (double)pixel_x * (2 - (-2)) / 1000;
-			c.y = 2 + (double)pixel_y * ((-2) - 2) / 1000;
-			int iter = calc_point(&c);
-			if (iter == 50)
-				better_pixel_put(&img, pixel_x, pixel_y, 000000);
-			else 
-				better_pixel_put(&img, pixel_x, pixel_y, 0x00FF0000);
-
+			c.x = -2 + pixel_x * (2.5 / 1000.0);
+			c.y = 2 - pixel_y * (4.0 / 1000.0);
+			int iter = calc_point(&c , max_iter);
+			color = generate_color(iter , max_iter);
+			better_pixel_put(&img, pixel_x, pixel_y, color);
 			pixel_y++;
 		}
 		pixel_x++;
